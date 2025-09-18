a252bc75e2f090376bb5998885d8c20a09723da9
Noogear <2364947161@qq.com>
2025-09-18 22:31:58 +0800

    Introduce MiniTag interface and refactor PapiTag
    Added the MiniTag interface for generic tag resolution. Renamed and refactored PapiTags to PapiTag, moving it to the impl package and implementing MiniTag<Player> for better modularity and clarity.

--- paper/adventure/src/main/java/gloomcore/paper/adventure/impl/PapiTag.java
+++ paper/adventure/src/main/java/gloomcore/paper/adventure/impl/PapiTag.java
@@ -1,5 +1,6 @@
-package gloomcore.paper.adventure;
+package gloomcore.paper.adventure.impl;
 
+import gloomcore.paper.adventure.MiniTag;
 import me.clip.placeholderapi.PlaceholderAPIPlugin;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import net.kyori.adventure.text.Component;
@@ -11,21 +12,22 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-public enum PapiTags {
+public enum PapiTag implements MiniTag<Player> {
     INSTANCE;
 
-    private final PlaceholderAPIPlugin papiPlugin = PlaceholderAPIPlugin.getInstance();
-    private final Map<String, PlaceholderExpansion> expansionCache = new ConcurrentHashMap<>();
+    private static final PlaceholderAPIPlugin PAPI_PLUGIN = PlaceholderAPIPlugin.getInstance();
+    private static final Map<String, PlaceholderExpansion> EXPANSION_CACHE = new ConcurrentHashMap<>();
 
-    public @NotNull TagResolver papiTag(final @NotNull Player player) {
-        return TagResolver.resolver("papi", (argumentQueue, context) -> {
+    @Override
+    public @NotNull TagResolver get(final @NotNull Player player) {
+        return TagResolver.resolver("papi", (argumentQueue, _) -> {
             final String placeholder = argumentQueue.popOr("papi tag requires an argument").value();
             final int index = placeholder.indexOf('_');
             if (index == -1) {
                 return Tag.inserting(Component.text(placeholder));
             }
-            final PlaceholderExpansion expansion = expansionCache.computeIfAbsent(placeholder.substring(0, index), name ->
-                    papiPlugin.getLocalExpansionManager().getExpansion(name)
+            final PlaceholderExpansion expansion = EXPANSION_CACHE.computeIfAbsent(placeholder.substring(0, index), name ->
+                    PAPI_PLUGIN.getLocalExpansionManager().getExpansion(name)
             );
             if (expansion == null) {
                 return Tag.inserting(Component.text(placeholder));
